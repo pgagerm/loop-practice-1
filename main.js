678a12ea7c017f64f2010772110fb374cf067ac1
Michael Germany <mike@rickgermany.com>
2015-01-19 15:56:00 -0700

    Final draft

---
+++ main.js
@@ -0,0 +1,33 @@
+// Loop Practice #1
+
+// for (var i = 0; i < 600; i = i + 100) {
+// 	console.log(i);
+// }
+
+// for (var i = 1; i < 128; i = i + i) {
+// 	console.log(i);
+// }
+
+// for (var i = 1; i < 4; i++) {
+// 	console.log(i);
+// 	console.log(i);
+// 	console.log(i);
+// }
+
+// for (var i = 0; i < 12; i = i + 2) {
+// 	console.log(i);
+// }
+
+// for (var i = 3; i < 18; i = i + 3) {
+// 	console.log(i);
+//  }
+
+// for (var i = 9; i >= 0; i = i - 1) {
+// 	console.log(i);
+// }
+
+for (var i = 0; i < 4; i++) {
+	for (x = 0; x < 4; x++) {
+		console.log(x);
+	}
+}
